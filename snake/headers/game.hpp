6287ca8250137439142a4025b58512218d540da0
Ehevi <aga.cyra@wp.pl>
2021-06-08 18:59:45 +0200

    back to the basics :innocent:

--- snake/headers/game.hpp
+++ snake/headers/game.hpp
@@ -1,38 +1,30 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
-#include "snakesection.hpp"
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
-#include <vector>
+
+#define SCREEN_HEIGHT 600
+#define SCREEN_WIDTH 800
 
 using namespace sf;
 using namespace std;
 
 class Game {
 private:
-    // Window
     Vector2f resolution;
     RenderWindow window;
     const unsigned int FPS = 60;
     static const Time TimePerFrame;
 
-    vector<SnakeSection> snake;
-
 public:
     Game();
 
-    void input();
+    void enableClose();
 
     void draw();
 
-    void newSnake();
-    void addSnakeSection();
-
-    // The main loop will be in the run function
     void run();
-
 };
-
 
 #endif //GAME_HPP
--- snake/src/game.cpp
+++ snake/src/game.cpp
@@ -3,62 +3,35 @@
 const sf::Time Game::TimePerFrame = seconds(1.f/60.f);
 
 Game::Game() {
-  resolution = Vector2f(800, 600);
-  window.create(VideoMode(resolution.x, resolution.y), "Hello My Snake Game", Style::Default);
+  resolution = Vector2f(SCREEN_WIDTH, SCREEN_WIDTH);
+  window.create(VideoMode(resolution.x, resolution.y), "Snake", Style::Default);
   window.setFramerateLimit(FPS);
+}
 
-  newSnake();
+void Game::run() {
+  while(window.isOpen()) {
+    enableClose();
+    draw();
+  }
 }
 
 void Game::draw() {
   window.clear(Color::Black);
 
-  // Draw snake sections
-  for (auto & s : snake) {
-    window.draw(s.getShape());
-  }
-
   window.display();
 }
 
-// This is the initial snake created at the start of each level
-void Game::newSnake() {
-  snake.clear();
-  snake.emplace_back(Vector2f(100,100));
-  snake.emplace_back(Vector2f(80,100));
-  snake.emplace_back(Vector2f(60,100));
-}
-
-void Game::input() {
+void Game::enableClose() {
   Event event{};
-
-  while (window.pollEvent(event)) {
-    // Window closed
-    if (event.type == Event::Closed) {
+  while(window.pollEvent(event)) {
+    if(event.type == Event::Closed) {
       window.close();
     }
 
-    // Handle Keyboard Input
-    if (event.type == Event::KeyPressed) {
-      // Quit
-      if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+    if(event.type == Event::KeyPressed) {
+      if(Keyboard::isKeyPressed(Keyboard::Escape)) {
         window.close();
       }
     }
-  }
-}
-
-void Game::addSnakeSection() {
-  // Just add the section at the same position as the last section of the snake, it will follow normally
-  Vector2f newSectionPosition = snake[snake.size() - 1].getPosition();
-  snake.emplace_back(newSectionPosition);
-}
-
-void Game::run() {
-
-  // Main loop - Runs until the window is closed
-  while (window.isOpen()) {
-    input();
-    draw();
   }
 }
